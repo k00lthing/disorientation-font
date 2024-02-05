8c467d89a55b3cf9d3e48be2f3d28bf48e7320c2
Christine Brovkina <christine@whateverism.de>
2024-02-05 14:03:47 +0100

    Adjust sidebearings in all masters

---
+++ disorientationsans_autospace.py
@@ -0,0 +1,17 @@
+# Script, Category, Subcategory, Case, Value, Reference Glyph, Filter
+
+# Letter
+*,Letter,*,smallCaps,1.2,h.sc,*,
+*,Letter,*,upper,1.25,H,*,
+*,Letter,*,lower,1.0,x,*,
+
+# Mark
+
+# Number
+*,Number,Decimal Digit,upper,1.25,H,*,
+*,Number,Decimal Digit,lower,1.25,x,*,
+*,Number,Decimal Digit,minor,0.8,one.dnom,.dnom,
+
+# Punctuation
+
+# Symbol
